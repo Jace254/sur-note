8c3cb15faadca1199915798e519f44e314ba3e80
Hypnos <47209391+Jace254@users.noreply.github.com>
2023-11-30 16:49:11 +0300

    chore: setup a visual style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import { Flex } from "@radix-ui/themes";
+import { Flex, Heading } from "@radix-ui/themes";
 import Nav from "./components/Nav";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import { MouseEvent } from "react";
 
 export default function App() {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  function handleMouseMove({ currentTarget, clientX, clientY }: MouseEvent) {
+    const { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
   return (
-    <div className="bg-gray-12 w-screen h-screen">
-      <Flex className="flex-col w-full">
-        <Nav/>
-        <section className="py-6 px-12">
-          <textarea className="text-brown-1 w-full bg-transparent caret-accent-5 h-auto border-none outline-none resize-none"></textarea>
-        </section>
+    <div
+      className="group relative flex bg-gray-12  justify-center h-screen w-screen text-bronze-7"
+      onMouseMove={handleMouseMove}
+    >
+      <Flex className="flex-col w-full sm:max-w-[460px]">
+        <motion.div
+          className="hidden sm:block pointer-events-none absolute max-w-full max-h-full -inset-px rounded-xl opacity-0 transition duration-300 group-hover:opacity-100"
+          style={{
+            background: useMotionTemplate`
+            radial-gradient(
+              300px circle at ${mouseX}px ${mouseY}px,
+              rgba(14, 165, 233, 0.15),
+              transparent 70%
+            )
+          `,
+          }}
+        />
+        <Nav />
+        <div className=" py-3 px-4">
+          <div className="mt-2 flex items-center gap-x-2">
+            <Heading className="font-bold tracking-tight text-bronze-6">
+              Hello
+            </Heading>
+          </div>
+          <p className="mt-6 text-base leading-7 font-sans line-clamp-none">
+            This is a minimal, browser-based text editor for people who are looking
+            for a minimalistic place to write their thouights on the internet, uninterrupted.
+          </p>
+        </div>
       </Flex>
     </div>
   );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,27 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import { Flex, Text, IconButton } from "@radix-ui/themes";
+import { Flex, IconButton, Link } from "@radix-ui/themes";
+import { CSSProperties } from "react";
+
 
 export default function Nav() {
   return (
-    <Flex className=" py-4 px-3 items-center row justify-between w-full border-b border-gray-11 border-opacity-50">
-      <Text className="font-medium text-brown-1 text-xl">Sur Editor</Text>
+    <Flex style={
+        {
+          "--background": "var(--gray-12)",
+          "--highlight": "255 255 255",
+
+          "--bg-color":
+            "linear-gradient(var(--background), var(--background))",
+          "--border-color": `linear-gradient(90deg,
+            rgb(var(--highlight) / 0.001) 0%,
+            rgb(var(--highlight) / 0.5) 33.33%,
+            rgb(var(--highlight) / 0.6) 66.67%,
+            rgb(var(--highlight) / 0.001) 100%)
+          `,
+        } as CSSProperties
+      }
+      className=" py-4 px-3 items-center row justify-between w-full border-b-2 border-transparent [background:padding-box_var(--bg-color),border-box_var(--border-color)]">
+      <Link className="font-mono text-brown-1 text-xl" href="/">Sur Editor</Link>
       <IconButton variant="soft" color="orange">
         <GitHubLogoIcon width="18" height="18" />
       </IconButton>
